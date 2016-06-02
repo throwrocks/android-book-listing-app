28b76b5a29aa3ef97100bccdf2ca436a7173cd5d
Jose E Lopez <p0p.thr0w@gmail.com>
2016-06-01 19:07:03 -0500

    Initial commit

---
+++ app/src/main/java/rocks/athrow/android_book_listing_app/BookListActivity.java
@@ -0,0 +1,13 @@
+package rocks.athrow.android_book_listing_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+    }
+}
